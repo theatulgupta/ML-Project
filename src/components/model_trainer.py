55a601ebc25875790606941d9e5dedbeff509e0f
theatulgupta <atulgupta3058@gmail.com>
2024-02-17 16:49:29 +0530

    Hyperparamter Training

--- src/components/model_trainer.py
+++ src/components/model_trainer.py
@@ -48,12 +48,56 @@ class ModelTrainer:
                 "AdaBoost Classifier":AdaBoostRegressor()
             }
             
+            params={
+                "Random Forest":{
+                    # 'criterion':['squared_error', 'friedman_mse', 'absolute_error', 'poisson'],"
+                    'n_estimators': [8,16,32,64,128,256],
+                    # 'max_features': ['auto', 'sqrt', 'log2'],
+                },
+                "Decision Tree": {
+                    'criterion':['squared_error', 'friedman_mse', 'absolute_error', 'poisson'],
+                    # 'splitter':['best', 'random'],
+                    # 'max_features':['auto', 'sqrt', 'log2']
+                },
+                "Gradient Boosting":{
+                    # 'loss':['squared_error', 'huber', 'absolute_error', 'quantile'],
+                    'learning_rate':[.1,.01,.05,.001],
+                    'subsample':[0.6,0.7,0.75,0.8,0.85,0.9],
+                    'n_estimators': [8,16,32,64,128,256],
+                    # 'max_features': ['auto', 'sqrt', 'log2'],
+                    # 'max_depth':[3,5,7,9,11,13,15]
+                },
+                "Linear Regression":{},
+                "K-Neighbors":{
+                    'n_neighbors':[5,7,9,11,13,15],
+                    # 'weights':['uniform','distance'],
+                },
+                "XGBClassifier":{
+                    'learning_rate':[.1,.01,.05,.001],
+                    'n_estimators': [8,16,32,64,128,256],
+                    # 'max_features': ['auto', 'sqrt', 'log2'],
+                    # 'max_depth':[3,5,7,9,11,13,15]
+                },
+                "CatBoosting Classifier":{
+                    'depth': [6,8,10],
+                    'learning_rate': [0.01, 0.05, 0.1],
+                    'iterations': [30, 50, 100]
+                    # 'loss_function': ['RMSE', 'MAE']
+                },
+                "AdaBoost Classifier":{
+                    'learning_rate':[.1,.01,0.5,.001],
+                    'n_estimators': [8,16,32,64,128,256]
+                    # 'loss_function': ['RMSE', 'MAE']
+                },  
+            }
+            
             model_report:dict=evaluate_models(
                 x_train=x_train,
                 y_train=y_train,
                 x_test=x_test,
                 y_test=y_test,
-                models=models
+                models=models,
+                params=params
             )
             
             # To get the best model score from dict
--- src/utils.py
+++ src/utils.py
@@ -5,6 +5,7 @@ import dill
 import numpy as np
 import pandas as pd
 from sklearn.metrics import r2_score
+from sklearn.model_selection import GridSearchCV
 
 from src.exception import CustomException
 
@@ -19,23 +20,32 @@ def save_object(file_path,object):
             
     except Exception as e:
         raise CustomException(e,sys)
-    
-def evaluate_models(x_train, y_train, x_test, y_test, models):
+
+def evaluate_models(x_train,y_train,x_test,y_test,models,params):
     try:
-        report = {}
+        report={}
         
-        for name, model in models.items():
-            model.fit(x_train, y_train)  # Training Model
+        for i in range(len(list(models))):
+            model=list(models.values())[i]
+            param=params[list(models.keys())[i]]
             
-            y_train_pred = model.predict(x_train)
-            y_test_pred = model.predict(x_test)
+            gs=GridSearchCV(model, param, cv=3)
+            gs.fit(x_train, y_train)
             
-            train_model_score = r2_score(y_train, y_train_pred)
-            test_model_score = r2_score(y_test, y_test_pred)
+            model.set_params(**gs.best_params_)
+            model.fit(x_train, y_train)
             
-            report[name] = test_model_score
+            # model.fit(x_train,y_train) # Training Model
+            
+            y_train_pred=model.predict(x_train)
+            y_test_pred=model.predict(x_test)
+            
+            train_model_score=r2_score(y_train,y_train_pred)
+            test_model_score=r2_score(y_test,y_test_pred)
+            
+            report[list(models.keys())[i]]=test_model_score
         
         return report
             
     except Exception as e:
-        raise CustomException(e, sys)
+        raise CustomException(e,sys)
